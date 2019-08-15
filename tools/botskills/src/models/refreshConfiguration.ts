95a4d76ce3d74b9466ee2f576bf3b5d988fcde3b
Martin Battaglino <martinbatta32@gmail.com>
2019-08-15 17:01:11 -0300

    Modularize methods and implement multilanguage in refresh

--- tools/botskills/src/models/refreshConfiguration.ts
+++ tools/botskills/src/models/refreshConfiguration.ts
@@ -6,9 +6,7 @@
 import { ILogger } from '../logger';
 
 export interface IRefreshConfiguration {
-    dispatchName: string;
     dispatchFolder: string;
-    language: string;
     luisFolder: string;
     lgLanguage: string;
     outFolder: string;
